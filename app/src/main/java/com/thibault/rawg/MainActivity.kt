07e24680c45a1765008791438dff9edf2b22b043
Thibault Scorielle <46597542+Genessy@users.noreply.github.com>
2024-04-03 15:57:46 +0200

    feat(koin)

--- app/src/main/java/com/thibault/rawg/MainActivity.kt
+++ app/src/main/java/com/thibault/rawg/MainActivity.kt
@@ -2,10 +2,15 @@ package com.thibault.rawg
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.koin.core.context.startKoin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startKoin{
+            modules()
+        }
     }
 }
